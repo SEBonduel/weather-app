f09353e01e7c8ab10b62e3ea7b5e77df15933595
SEBonduel <sebastien050599@hotmail.fr>
2025-02-12 03:47:52 +0100

    "x" postion

--- src/Navbar.js
+++ src/Navbar.js
@@ -90,7 +90,7 @@ const Navbar = ({ onSearch }) => {
           className="ml-1 w-32 lg:w-full md:w-full sm:w-full px-4 py-2 rounded-full bg-white text-black focus:outline-none"
         />
         {city && (
-          <button onClick={handleClear} className="absolute right-3 text-gray-500 hover:text-gray-700">
+          <button onClick={handleClear} className="absolute right-1 lg:right-3 md:right-3 sm:right-3 text-gray-500 hover:text-gray-700">
             <X size={20} />
           </button>
         )}
